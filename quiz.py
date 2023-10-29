0ae21ed98912274c915706ee1d0d9c2c49fce4ca
qazkacper12 <qazkacper12@gmail.com>
2023-10-29 09:19:59 -0500

    quzi
    quiz

---
+++ quiz.py
@@ -0,0 +1,386 @@
+#menu
+print("Witaj w Quizie który odciągnie cię od kibla")
+
+play = input("""(1) Graj
+(2) Wyjdź
+>>> """)
+print()
+if play != "1":
+    quit()
+
+score = 0
+#pytanie 1    
+answer = input("""Pytanie 1:
+Co to linux?
+(1) Program
+(2) System operacyjny
+(3) Firma produkująca srajtaśme
+>>> """)
+
+if answer == "2":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 2
+          
+          """)
+
+#pytanie 2
+answer = input("""Pytanie 2:
+Kto to BRYŚ?
+(1) Kot
+(2) Pies
+(3) Firma produkująca srajtaśme
+>>> """)
+
+if answer == "1":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 1
+          
+          """)
+
+#pytanie 3
+answer = input("""Pytanie 3:
+Kto to Skubi?
+(1) Kot
+(2) Pies
+(3) Firma produkująca srajtaśme
+>>> """)
+
+if answer == "2":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 2
+          
+          """)
+    
+#pytanie 4
+answer = input("""Pytanie 4:
+Gdzie leży Krym?
+(1) Na ukrainie
+(2) W rosji
+(3) Na ciostku
+>>> """)
+
+if answer == "3":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 3
+          
+          """)
+    
+#pytanie 5
+answer = input("""Pytanie 5:
+Co to Granat?
+(1) Owoc
+(2) Kolor
+(3) Broń miotana
+(4) Wyszystkie powyższe
+>>> """)
+
+if answer == "4":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 4
+          
+          """)
+    
+
+#pytanie 6
+answer = input("""Pytanie 6:
+Jaki smak przeważa w kwaśnicy?
+(1) Słodki
+(2) Mięsny
+(3) Kwaśny
+>>> """)
+
+if answer == "3":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 3
+          
+          """)
+    
+#pytanie 7
+answer = input("""Pytanie 7:
+Czy Święty Mikołaj istnieje?
+(1) Tak
+(2) Nie
+(3) Nie wiem
+>>> """)
+
+if answer == "1":
+    print("""
+        DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    ŚWIĘTY MIKOŁAJ ISTNIEJE!
+          
+          """)
+    
+#pytanie 8
+answer = input("""Pytanie 8:
+Czym jedzie żul i co jedzie żulem?
+(1) Tramwaj
+(2) Autobus
+(3) Pociąg
+>>> """)
+
+if answer == "2":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 2
+          
+          """)
+
+
+#pytanie 9
+answer = input("""Pytanie 9:
+Co to window?
+(1) System operacyjny
+(2) Drzwi
+(3) Okno
+>>> """)
+
+if answer == "3":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 3
+          
+          """)
+    
+
+#pytanie 10
+answer = input("""Pytanie 10:
+O czym jest "Mały Książe"?
+(1) O Żulu
+(2) O przyjaźni
+(3) O Małym Księciu
+>>> """)
+
+if answer == "1":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 1
+          
+          """)
+    
+
+#pytanie 11
+answer = input("""Pytanie 11:
+Kim był Osama Bin Laden"?
+(1) Znanym menelem
+(2) Terrorystą
+(3) gumisiem
+>>> """)
+
+if answer == "2":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 2
+          
+          """)
+    
+
+#pytanie 12
+answer = input("""Pytanie 12:
+Jakie są atrybuty Menela?
+(1) Piwne Berło
+(2) Broda z miliardem żyjątek
+(3) Obrzygane ubrania
+(4) Wszystkie powyższe
+>>> """)
+
+if answer == "4":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 4
+          
+          """)
+
+#pytanie 13
+answer = input("""Pytanie 8:
+Jakiej narodowości był Jan Paweł II?
+(1) Żydem
+(2) Polakiem
+(3) Niemcem
+>>> """)
+
+if answer == "2":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 2
+          
+          """)
+
+#pytanie 14
+answer = input("""Pytanie 14:
+Co to czopek?
+(1) Lek wkładany do dupy
+(2) Firma samochodowa
+(3) Tabletka na tycie
+>>> """)
+
+if answer == "1":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 1
+          
+          """)
+
+
+#pytanie 15
+answer = input("""Pytanie 15:
+co to znaczy ""else" w pythonie?
+(1) Albo
+(2) Zmienna
+(3) W przeciwnym razie
+>>> """)
+
+if answer == "3":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 3
+          
+          """)
+
+#pytanie 16
+answer = input("""Pytanie 16:
+Kim był CJ z GTA?
+(1) Żydem
+(2) Murzynem
+(3) Arabem
+>>> """)
+
+if answer == "2":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 2
+          
+          """)
+
+#pytanie 17
+answer = input("""Pytanie 17:
+Kto powiedział "Follow the train CJ!"?
+(1) Policjant
+(2) Lance Wilson
+(3) Sean Johnson
+(4) Kane
+(5) Big Smoke
+(6) Catalina
+>>> """)
+
+if answer == "5":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 5
+          
+          """)
+    
+
+#pytanie 18
+answer = input("""Pytanie 18:
+Ulubiona gra Klocucha: 
+(1) GTA 5
+(2) Cyberpunk 2077
+(3) GTA SA
+>>> """)
+
+if answer == "3":
+    print("""
+          DOBRZE
+          """)
+    score += 1
+else:
+    print("""
+          Źle
+    Poprawna odpowiedź to: 3
+          
+          """)
+
+
+#podsumowanie punktów
+print("Masz " + str(score) + " z 18 porawnych odpowiedzi. A TERAZ WRACAJ NA KIBEL")
